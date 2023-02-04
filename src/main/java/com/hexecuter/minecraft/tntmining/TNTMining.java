4d25fe04c4947c8d9c3d4c125bce27901c498fd7
BuildTools <ealvarez2025@gmail.com>
2023-02-03 23:00:42 -0700

    Create helper function for destroying blocks in a cuboid radius Also created an enumset that contains all the blocks the tnt is allowed to break

--- src/main/java/com/hexecuter/minecraft/tntmining/TNTMining.java
+++ src/main/java/com/hexecuter/minecraft/tntmining/TNTMining.java
@@ -8,23 +8,68 @@ import java.util.EnumSet;
 
 public final class TNTMining extends JavaPlugin {
     public static TNTMining instance;
-    public static EnumSet<Material> ALL_MATERIALS;
+    public static EnumSet<Material> ALL_BLOCKS;
+    public static EnumSet<Material> BLOCKS_TO_BREAK;
 
     @Override
     public void onEnable() {
         instance = this;
-        ALL_MATERIALS = EnumSet.noneOf(Material.class);
+
+        ALL_BLOCKS = EnumSet.noneOf(Material.class);
         for (Material currentMaterial : Material.values()) {
             if (currentMaterial.isBlock()) {
-                ALL_MATERIALS.add(currentMaterial);
+                ALL_BLOCKS.add(currentMaterial);
             }
         }
-        getServer().getPluginManager().registerEvents(new TntLeftClickListener(), this);
 
+        populateBlocksToBreak();
+
+        getServer().getPluginManager().registerEvents(new TntLeftClickListener(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    // TODO: eventually change this to loading in a config file
+    public static void populateBlocksToBreak() {
+        BLOCKS_TO_BREAK.add(Material.COARSE_DIRT);
+        BLOCKS_TO_BREAK.add(Material.DIRT);
+        BLOCKS_TO_BREAK.add(Material.DIRT_PATH);
+        BLOCKS_TO_BREAK.add(Material.ROOTED_DIRT);
+        BLOCKS_TO_BREAK.add(Material.COBBLESTONE);
+        BLOCKS_TO_BREAK.add(Material.COBBLED_DEEPSLATE);
+        BLOCKS_TO_BREAK.add(Material.DEEPSLATE);
+        BLOCKS_TO_BREAK.add(Material.SAND);
+        BLOCKS_TO_BREAK.add(Material.SANDSTONE);
+        BLOCKS_TO_BREAK.add(Material.STONE);
+        BLOCKS_TO_BREAK.add(Material.GRAVEL);
+        BLOCKS_TO_BREAK.add(Material.PODZOL);
+        BLOCKS_TO_BREAK.add(Material.RED_SAND);
+        BLOCKS_TO_BREAK.add(Material.RED_SANDSTONE);
+        BLOCKS_TO_BREAK.add(Material.DIORITE);
+        BLOCKS_TO_BREAK.add(Material.CLAY);
+        BLOCKS_TO_BREAK.add(Material.ANDESITE);
+        BLOCKS_TO_BREAK.add(Material.BLACK_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.BLUE_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.BROWN_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.CYAN_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.GRAY_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.GREEN_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.LIGHT_BLUE_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.LIGHT_GRAY_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.LIME_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.MAGENTA_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.ORANGE_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.PINK_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.PURPLE_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.RED_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.WHITE_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.YELLOW_TERRACOTTA);
+        BLOCKS_TO_BREAK.add(Material.WATER);
+        BLOCKS_TO_BREAK.add(Material.LAVA);
+        BLOCKS_TO_BREAK.add(Material.NETHERRACK);
     }
 }
--- src/main/java/com/hexecuter/minecraft/tntmining/helpers/HelperFunctions.java
+++ src/main/java/com/hexecuter/minecraft/tntmining/helpers/HelperFunctions.java
@@ -10,12 +10,29 @@ public class HelperFunctions {
     public static ArrayList<Block> getBlocksInLineOfSight(LivingEntity entity, int distance) {
         ArrayList<Block> blockList = new ArrayList<>();
         int counter = 0;
-        for (Block currentBlock : entity.getLineOfSight(TNTMining.ALL_MATERIALS, distance)) {
+        for (Block currentBlock : entity.getLineOfSight(TNTMining.ALL_BLOCKS, distance)) {
             if (counter >= 2) {
                 blockList.add(currentBlock);
             }
             counter++;
         }
         return blockList;
+    }
+
+    public static void removeBlocks(Block block, int radius) {
+        int endingX = block.getX() + radius;
+        int endingY = block.getY() + radius;
+        int endingZ = block.getZ() + radius;
+
+        for (int startingX = block.getX() - radius; startingX < endingX; startingX++){
+            for(int startingY = block.getY() - radius; startingY < endingY; startingY++) {
+                for (int startingZ = block.getZ() - radius; startingZ < endingZ; startingZ++) {
+                    Block currentBlock = block.getWorld().getBlockAt(startingX, startingY, startingZ);
+                    if (TNTMining.BLOCKS_TO_BREAK.contains(currentBlock.getType())) {
+                        currentBlock.breakNaturally();
+                    }
+                }
+            }
+        }
     }
 }
